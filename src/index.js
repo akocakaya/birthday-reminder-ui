c237293b5993abd1118d4301c274baf49479f037
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2022-03-14 00:41:58 +0300

    add: ssr render html flow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const express = require('express');
+import express          from 'express';
+import React            from 'react';
+import ReactDOMServer   from 'react-dom/server';
+
+import SignUp           from './client/pages/auth/sign-up.js';
+
 const app = express();
+
+app.get('/', (_req, res) => {
+    const content = ReactDOMServer.renderToString(<SignUp />);
+
+    res.send(content);
+});
 
 const applicationPort = 3000;
 app.listen(applicationPort, () => {

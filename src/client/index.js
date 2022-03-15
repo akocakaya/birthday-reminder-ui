b076c48fa55410e8ff8bc92a3220aa88b8b66358
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2022-03-15 15:47:30 +0300

    add: react app with router to client

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import React                from 'react';
+import ReactDOM             from 'react-dom';
+import { BrowserRouter }    from 'react-router-dom';
+
+import AppRouter            from './router/index.js';
+
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <AppRouter />
+    </BrowserRouter>, 
+    document.querySelector('#root'),
+);

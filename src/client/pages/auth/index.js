9d61690d31f8336a7cd51df354eb330652cced74
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2022-03-15 15:48:17 +0300

    add: react, router dep

---
+++ src/client/pages/auth/index.js
@@ -0,0 +1,5 @@
+import SignUp from "./sign-up";
+
+export {
+    SignUp,
+};
--- webpack.server.js
+++ webpack.server.js
@@ -41,7 +41,6 @@ module.exports = {
             },
         ],
     },
-
     externals: [
         webpackNodeExternals(),
     ],

2cb58faaf049c1c96323df77b64e55bbcff047a9
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2022-03-19 00:38:18 +0300

    add: redux to client

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,29 @@
-import React                from 'react';
-import ReactDOM             from 'react-dom';
-import { BrowserRouter }    from 'react-router-dom';
+import '@babel/polyfill';
+import React                            from 'react';
+import ReactDOM                         from 'react-dom';
+import { BrowserRouter }                from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider }                     from 'react-redux';
+import createSagaMiddleware             from 'redux-saga';
 
-import App            from '../app/index.js';
+import App                              from '../app/index.js';
+import reducer                          from '../reducer/index.js';
+import appSaga                          from '../app/saga/index.js';
+
+const initialState = {};
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    reducer,
+    initialState,
+    applyMiddleware(sagaMiddleware),
+);
+sagaMiddleware.run(appSaga);
 
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>, 
     document.querySelector('#root'),
 );

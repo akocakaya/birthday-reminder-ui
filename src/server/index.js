63ebfffaa0e1c657e77aec78af62ceffe2470770
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2022-03-19 00:40:15 +0300

    add: router to server WIP

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,6 @@
+import '@babel/polyfill';
 import express          from 'express';
+import bodyParser       from 'body-parser';
 
 import * as Utils       from './utils/index.js';
 
@@ -17,3 +19,17 @@ app.listen(applicationPort, () => {
     console.log(`Listening on port ${applicationPort}`);
     console.log(`Node version: ${process.versions.node}`);
 });
+
+app.use(bodyParser.json());
+
+// refactor
+const router = express.Router();
+router.route('/api/auth/sign-up')
+    .post( (req, res, _next) => {
+        console.log(`req.body -> `, req.body)
+        res.json(
+            req.body,
+        );
+    });
+
+app.use('/', router);

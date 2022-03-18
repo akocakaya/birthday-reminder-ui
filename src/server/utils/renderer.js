cc35b86b78129193596c03b77fc423880a4a4a7d
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2022-03-19 00:34:35 +0300

    add: redux store to server

--- src/server/utils/renderer.js
+++ src/server/utils/renderer.js
@@ -1,15 +1,25 @@
 import React                from 'react';
 import ReactDOMServer       from 'react-dom/server';
 import { StaticRouter }     from 'react-router';
+import { Provider }         from 'react-redux'; 
+import { createStore }      from 'redux';
 
 import App                  from '../../app/index.js';
+import reducer              from '../../reducer/index.js';
 
 export default req => {
+    // store creation for server
+    const store = createStore(
+        reducer
+    );
+
     const context = {};
     const content = ReactDOMServer.renderToString(
-        <StaticRouter location={req.path} context={context}>
-            <App />
-        </StaticRouter>
+        <Provider store={store}>
+            <StaticRouter location={req.path} context={context}>
+                <App />
+            </StaticRouter>
+        </Provider>
     );
 
     const template = `

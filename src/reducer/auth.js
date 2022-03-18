ba4096a9e9890d7fee8feec15fd8f8f210690fb0
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2022-03-19 00:35:15 +0300

    add: auth reducer

---
+++ src/reducer/auth.js
@@ -0,0 +1,10 @@
+export const AUTH_SIGN_UP = 'AUTH_SIGN_UP';
+
+const initialState = {};
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    };
+};

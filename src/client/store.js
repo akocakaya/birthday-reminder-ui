9ab336b9867541420cd1329fbfe2b074e578b865
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2022-03-23 19:22:30 +0300

    add: sent filled initial state from server to client

--- src/client/store.js
+++ src/client/store.js
@@ -5,7 +5,7 @@ import reducer                          from '../reducer/index.js';
 import appSaga                          from '../app/saga/index.js';
 
 export default () => {
-    const initialState = {};
+    const initialState = window.INITIAL_STATE;
     const sagaMiddleware = createSagaMiddleware();
     const store = createStore(
         reducer,
